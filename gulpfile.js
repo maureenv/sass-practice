61f843339b5be085be828e8d7907b0218eca940c
Maureen VOGEL <maureenvogel@MaureenOGELsMBP.home>
2016-05-26 11:18:30 -0400

    Fixing home page information containers

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('task-name', function(){
+  // say gulp, there's a task, and 'task-name' will be the name of it
+  console.log("Hello");
+
+  // to see the words hello go into terminal and type in gulp task-name
+});
+
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) //refering to var sass
+    .pipe(gulp.dest('app/css'))
+})
